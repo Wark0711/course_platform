74b7b92a9d2b24f6e77b2c92dabb451ba784db17
Wark0711 <mickey.rahul98@gmail.com>
2025-02-07 22:28:25 +0530

    Added clerk user auth and made adjustments

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         NEON_DB_URL: z.string().url(),
+        CLERK_SECRET_KEY: z.string().min(1),
     },
     experimental__runtimeEnv: process.env
 });
