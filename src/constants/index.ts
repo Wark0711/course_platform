fb17df14716f70014627a4e606bb5a8662398faa
Wark0711 <mickey.rahul98@gmail.com>
2025-02-08 22:55:38 +0530

    Added signin and signup pages and updated homepage

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export const navigationLinks = [
+    {
+        href: "/courses",
+        label: "My Courses",
+    },
+    {
+        href: "/purchases",
+        label: "My Purchases",
+    },
+];
