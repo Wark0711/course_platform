e0a4ab64f4f81bfb33462ed153b10f4f6e19ff89
Wark0711 <mickey.rahul98@gmail.com>
2025-02-09 00:18:02 +0530

    Added webhook feature using clerk to connect with neon db

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
     server: {
         NEON_DB_URL: z.string().url(),
         CLERK_SECRET_KEY: z.string().min(1),
+        CLERK_WEBHOOK_SECRET: z.string().min(1)
     },
     experimental__runtimeEnv: process.env
 });
