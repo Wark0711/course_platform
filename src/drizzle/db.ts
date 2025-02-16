ede3bcc8d1b04f93d7585765e41159e66afed316
Wark0711 <mickey.rahul98@gmail.com>
2025-02-17 00:52:45 +0530

    Completed lesson features and changed neon db connection type and added YT video frature

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,11 @@
 import { env } from '@/data/env/server';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import * as schema from "./schema"
 
-const sql = neon(env.NEON_DB_URL!);
+// const sql = neon(env.NEON_DB_URL!);
 
-export const db = drizzle(sql, { schema, logger: true })
+export const db = drizzle({
+    connection: { connectionString: env.NEON_DB_URL! },
+    schema
+})
