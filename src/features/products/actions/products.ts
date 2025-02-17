290e5948735b873aa257ad4ad719e278f1f3a894
Wark0711 <mickey.rahul98@gmail.com>
2025-02-17 22:53:37 +0530

    Added layout for products page and add new product page, along with some backend section

---
+++ src/features/products/actions/products.ts
@@ -0,0 +1,39 @@
+"use server"
+
+import { z } from "zod"
+import { redirect } from "next/navigation"
+import { getCurrentUser } from "@/services/clerk"
+import { insertProduct, modifyProduct, removeProduct } from "../db/products"
+import { canCreateProducts, canDeleteProducts, canUpdateProducts } from "../permissions/products"
+import { productSchema } from "../schema/products"
+
+export async function createProduct(unsafeData: z.infer<typeof productSchema>) {
+    const { success, data } = productSchema.safeParse(unsafeData)
+
+    if (!success || !canCreateProducts(await getCurrentUser())) {
+        return { error: true, message: "There was an error creating your product" }
+    }
+
+    await insertProduct(data)
+    redirect("/admin/products")
+}
+
+export async function updateProduct(id: string, unsafeData: z.infer<typeof productSchema>) {
+    const { success, data } = productSchema.safeParse(unsafeData)
+
+    if (!success || !canUpdateProducts(await getCurrentUser())) {
+        return { error: true, message: "There was an error updating your product" }
+    }
+
+    await modifyProduct(id, data)
+    redirect("/admin/products")
+}
+
+export async function deleteProduct(id: string) {
+    if (!canDeleteProducts(await getCurrentUser())) {
+        return { error: true, message: "Error deleting your product" }
+    }
+
+    await removeProduct(id)
+    return { error: false, message: "Successfully deleted your product" }
+}
